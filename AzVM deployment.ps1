21c01911d2b136699b0951c8395dbdd387429a82
Nikhil Kenvetil <44366323+nikhilkenvetil@users.noreply.github.com>
2022-12-22 21:10:14 +0530

    Add files via upload

---
+++ AzVM deployment.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+param (
+
+    [Parameter()]
+    [string]
+    $RGName,
+
+    [Parameter()]
+    [string]
+    $dnsLabelPrefix,
+
+    [Parameter()]
+    [string]
+    $adminUsername,
+
+    [Parameter()]
+    [SecureString]
+    $adminPassword
+)
+
+$adminPassword = ConvertTo-SecureString "Qwertyuiop1234!" -AsPlainText -Force
+$adminUsername = "nikhil"
+$dnsLabelPrefix = "nkhl"
+$RGName = "Sonar_test"
+$location = "South India"
+
+Connect-AzAccount
+Set-AzContext -Subscription "1b528990-0c0e-475f-85b2-4ccffe89f9ce"
+New-AzResourceGroup -Name $RGName -Location $location
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $RGName `
+    -TemplateFile C:\nkhl\Projects\CreateSonarQubeVM.json `
+    -adminUsername $adminUsername `
+    -adminPassword $adminPassword `
+    -dnsLabelPrefix $dnsLabelPrefix `
+    -OSVersion "2022-datacenter" `
+    -vmName "SonarQubeServer"
+
+(Get-AzVm -ResourceGroupName $RGName).name
