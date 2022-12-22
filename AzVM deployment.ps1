ccc8fd60fb38122167f69437b0004b29365fd36f
Nikhil Kenvetil <44366323+nikhilkenvetil@users.noreply.github.com>
2022-12-23 00:40:55 +0530

    Update AzVM deployment.ps1

--- AzVM deployment.ps1
+++ AzVM deployment.ps1
@@ -18,14 +18,15 @@ param (
     $adminPassword
 )
 
-$pwd = $adminPassword -AsSecureString
+$pwd = $adminPassword
+# $pwd = ConvertTo-SecureString $adminPassword -AsPlainText -Force
 # $adminUsername = "nikhil"
 # $dnsLabelPrefix = "nkhl"
 # $RGName = "Sonar_test"
 $location = "South India"
 
-Connect-AzAccount
-Set-AzContext -Subscription "1b528990-0c0e-475f-85b2-4ccffe89f9ce"
+# Connect-AzAccount
+# Set-AzContext -Subscription "1b528990-0c0e-475f-85b2-4ccffe89f9ce"
 New-AzResourceGroup -Name $RGName -Location $location
 
 New-AzResourceGroupDeployment `
